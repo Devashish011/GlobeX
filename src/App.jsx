2e3feaebbcd38d7d35f615090212758e4de5aa23
Devashish011 <devashishrathore2019@gmail.com>
2024-02-22 15:44:14 +0530

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import Homepage from "./Pages/Homepage";
+import Pricing from "./Pages/Pricing";
+import Product from "./Pages/Product";
+import PageNotFound from "./Pages/PageNotFound";
+import AppLayout from "./Pages/AppLayout";
+import Login from "./Pages/Login";
+import CityList from "./componenets/CityList";
+import CountryList from "./componenets/CountryList";
+import City from "./componenets/City";
+import Form from "./componenets/Form";
+import { CitiesProvider } from "./Contexts/CitiesContext";
+import { AuthProvider } from "./Contexts/FakeAuthContext";
+import ProtectedRoute from "./Pages/ProtectedRoute";
+
+function App() {
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route
+                index
+                element={<Navigate replace to="cities" />} //resirect to cities route
+              />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
